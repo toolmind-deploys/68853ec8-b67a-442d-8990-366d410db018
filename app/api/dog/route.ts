b0c60ca72ed45a7b39dc7e7fce4882902e7f7ee9
toolmind-deploys <toolminddeploys@gmail.com>
2025-06-14 14:48:29 -0700

    chore: add app/api/dog/route.ts

---
+++ app/api/dog/route.ts
@@ -0,0 +1,40 @@
+import { firestore } from "firebase-admin";
+import { initFirebaseAdminSDK } from "@/config/firebase-admin-config";
+import { NextRequest, NextResponse } from "next/server";
+
+initFirebaseAdminSDK();
+
+const fsdb = firestore();
+
+export async function GET() {
+  try {
+    const snapshot = await fsdb.collection("feeds").get();
+    const data = snapshot.docs.map((doc) => doc.data());
+    return NextResponse.json({ data });
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    if (!body.id) {
+      return NextResponse.json(
+        { error: "Missing 'id' field." },
+        { status: 400 }
+      );
+    }
+
+    await fsdb.collection("feeds").doc(body.id).set(body);
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
